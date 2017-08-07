166859d216340225483f27d91fc47bded619b485
leon in 't Veld <leon3110l@gmail.com>
2017-08-07 14:32:02 +0200

    added build script and fixed some stuff

---
+++ build.sh
@@ -0,0 +1,3 @@
+# copy the theme to the right folder
+rm -r /usr/share/lightdm-webkit/themes/lightdm-webkit-theme
+cp -r ../lightdm-webkit-theme /usr/share/lightdm-webkit/themes/
--- js/main.js
+++ js/main.js
@@ -1,24 +1,54 @@
-// clock stuff
+window.addEventListener("load", () => {
+  const wmSelect = document.querySelector(".wmSelect");
+  const userSelect = document.querySelector(".userSelect");
 
-const timer = document.querySelector(".time");
-updateTime();
-setInterval(() => {
+  // clock stuff
+  const timer = document.querySelector(".time");
   updateTime();
-}, 30000);
+  setInterval(() => {
+    updateTime();
+  }, 30000);
 
-function updateTime() {
-  timer.innerHTML = parseTime(new Date(), "%h:%m");
-}
-
-function parseTime(date, format) {
-  const seconds = addZero(date.getSeconds());
-  const minutes = addZero(date.getMinutes());
-  const hours = addZero(date.getHours());
-  function addZero(val) {
-    if (val < 10) {
-      return "0"+val;
-    }
-    return val;
+  function updateTime() {
+    timer.innerHTML = theme_utils.get_current_localized_time();
   }
-  return format.replace("%h", hours).replace("%m", minutes).replace("%s", seconds);
-}
+
+  // get user data
+  for (let user in lightdm.users) {
+    console.log(user);
+    const option = document.createElement("option");
+    option.value = user.username;
+    option.innerHTML = user.username;
+    userSelect.appendChild(option);
+  }
+  for (let session in lightdm.sessions) {
+    console.log(session);
+    const option = document.createElement("option");
+    option.value = session.key;
+    option.innerHTML = session.name;
+    wmSelect.appendChild(option);
+  }
+
+  // event listeners
+  document.querySelector(".power").addEventListener("click", (e) => {
+    lightdm.shutdown();
+  });
+
+  document.querySelector(".reboot").addEventListener("click", (e) => {
+    lightdm.restart();
+  });
+
+  userSelect.addEventListener("click", e => {
+    const user = lightdm.users.find(x => x.username === e.target.value);
+    for (let i = 0; i < wmSelect.options.length; i++) {
+      if (wmSelect.options[i].value === user.session) {
+        break;
+      }
+    }
+    wmSelect.options.selectedIndex = i || 0;
+  });
+
+  document.querySelector(".form").addEventListener("submit", e => {
+    const pass = document.querySelector(".password").value;
+  });
+});
